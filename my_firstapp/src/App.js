d921607bd19dc4a6af36d349a244b9b4e8ac7579
anil <anilrana72149@gmail.com>
2024-09-15 21:47:09 +0530

    adding all file

---
+++ my_firstapp/src/App.js
@@ -0,0 +1,18 @@
+
+
+function App() {
+  return (
+    <button>HIIII</button>
+  );
+}
+
+export default App;
+
+
+function Myfunction(){
+ return(
+  <button>I am a buttom</button>
+ )
+} 
+
+export default Myfunction
---
+++ my_firstapp/src/demo.js
@@ -0,0 +1,21 @@
+function Demo(){
+    return(
+        <h1>Hello!This is the demo of reactjs </h1>
+    );
+}
+export default Demo
+
+function Button(){
+    return(
+     <button>I am a buttom</button>
+    )
+   } 
+
+   export default function MyApp() {
+    return (
+      <div>
+        <h1>Welcome to my app</h1>
+        <MyButton />
+      </div>
+    );
+  }
